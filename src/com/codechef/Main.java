f5a501ff1eb85f4ad415ffb957b9a1ae0f4ef8b8
Nitin <nsinghboss02@gmail.com>
2021-12-05 11:35:10 +0530

    replaced initial 1 with 0

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -11,10 +11,10 @@ public class Main {
    * * * * *
  */
 for(int i=1;i<=5;i++){
-	for(int k=0;k<=5-i+1;k++){
+	for(int k=1;k<=5-i+1;k++){
 		System.out.print(" ");
 	}
-	for(int j=0;j<=i;j++){
+	for(int j=1;j<=i;j++){
 		System.out.print("*");
 	}
 	System.out.println();
